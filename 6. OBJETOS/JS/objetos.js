87861f8e76b4278fef39f969d3cae1e309960e22
Albertobll97 <albertobll97@alumnostrazos.net>
2024-07-10 10:35:27 +0200

    nose

--- 6. OBJETOS/JS/objetos.js
+++ 6. OBJETOS/JS/objetos.js
@@ -17,7 +17,6 @@
 
 // - Con este objeto imprime por consola una pequeña historia del usuario, "Me llamo John Doe, tengo 35 años..."
 
-
 const userHistoria = {
   name: 'John',
   surname: 'Doe',
@@ -38,92 +37,143 @@ const userHistoria = {
   email: 'johndoe@example.com',
   occupation: 'Ingeniero de software',
   education: 'Master en ciencia de datos'
+};
 
+console.log(
+  `Hola me llamo ${userHistoria.name} ${userHistoria.surname}  tengo ${userHistoria.age} mis hobbies son ${userHistoria.hobbies}. Vivo en la C/${userHistoria.address.street} en la ciudad de  ${userHistoria.address.city} en el estado de ${userHistoria.address.state} mi codigo postal es ${userHistoria.address.postalCode} en ${userHistoria.address.country}. Mi trabajo es ${userHistoria.occupation} y estudie  ${userHistoria.education}. Mis datos de contacto son, NUMERO:${userHistoria.phone}, EMAIL:${userHistoria.email}. Tengo dos mascotas un ${userHistoria.pets[0].type} llamado ${userHistoria.pets[0].name} y un ${userHistoria.pets[1].type} llamado ${userHistoria.pets[1].name}`
   
-}
-
-console.log (`Hola me llamo ${userHistoria.name} ${userHistoria.surname}  tengo ${userHistoria.age} mis hobbies son ${userHistoria.hobbies}. Vivo en la C/${userHistoria.address.street} en la ciudad de  ${userHistoria.address.city} en el estado de ${userHistoria.address.state} mi codigo postal es ${userHistoria.address.postalCode} en ${userHistoria.address.country}. Mi trabajo es ${userHistoria.occupation} y estudie  ${userHistoria.education}. Mis datos de contacto son, NUMERO:${userHistoria.phone}, EMAIL:${userHistoria.email}. Tengo dos mascotas un ${userHistoria.pets[0].type} llamado ${userHistoria.pets[0].name} y un ${userHistoria.pets[1].type} llamado ${userHistoria.pets[1].name}` );
+);
 
 
 // - Dado este objeto, rellena los 5 arrays con el array de numbers. número + 2, número x 2, número / 2, números pares y números impares.
 
 // //javascript
-// const data = {
-//   numbers: [10, 32, 31, 67, 9, 2, 51, 4],
-//   firstFloor: {
-//     secondFloor: {
-//       numbersPlus2: []
-//     },
-//     thirdFloor: {
-//       numbersDouble: []
-//     }
-//   },
-//   fourthFloor: {
-//     numbersDividedBy2: []
-//   },
-//   fifthFloor: {
-//     onlyEven: [],
-//     onlyOdd: []
-//   }
-// };
-// ```
+
+const data = {
+  numbers: [10, 32, 31, 67, 9, 2, 51, 4],
+  firstFloor: {
+    secondFloor: {
+      numbersPlus2: []
+    },
+    thirdFloor: {
+      numbersDouble: []
+    }
+  },
+  fourthFloor: {
+    numbersDividedBy2: []
+  },
+  fifthFloor: {
+    onlyEven: [],
+    onlyOdd: []
+  }
+};
+
+const dataObject = number => {
+  for (const number of  data.numbers) {
+    data.firstFloor.secondFloor.numbersPlus2.push(number + 2);
+    data.firstFloor.thirdFloor.numbersDouble.push(number * 2);
+    data.fourthFloor.numbersDividedBy2.push(number / 2);
+
+    if (number % 2 === 0) {
+      data.fifthFloor.onlyEven.push(number);
+    } else {
+      data.fifthFloor.onlyOdd.push(number);
+    }
+  }
+  console.log(data.firstFloor.secondFloor.numbersPlus2);
+  console.log(data.firstFloor.thirdFloor.numbersDouble);
+  console.log(data.fourthFloor.numbersDividedBy2)
+  console.log(data.fifthFloor.onlyEven)
+  console.log(data.fifthFloor.onlyOdd)
+
+};
+dataObject(data.numbers)
+
+
+
+
+
+
+
 
 // - Crea una función que reciba una frase, por ejemplo "Si no estudias acabarás como Victor", y rellena el objeto con valores que te pide. Revisa la documentación de los strings si hay algo que no sabes obtener.
 //   https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String
 
-// const wordArray = phrase => {
-//   const vowels = 'aeiouáéíóú';
-  
-//   for (const letter of phrase) {
-//   if (vowels.includes(letter)) {
-//   data2.firstFloor.vowels.push(letter);
-//   } else if (letter !== ' ') {
-//   data2.secondFloor.consonants.push(letter);
-//   }
-  
-//   data2.fourthFloor.asciiCode.push(letter.charCodeAt());
-//   }
-  
-//   data2.fifthFloor.wordsInUppercase = phrase.toUpperCase().split(' ');
-//   data2.fifthFloor.wordsInLowercase = phrase.toLowerCase().split(' ');
-  
-  // En este nivel codificarás la frase para que sea un secreto.
-  // Si el caracter es una vocal, la sustituirás por un número siendo a-1 e-2 i-3 o-4 u-5
-  // Si el caracter es una consonante deberás sustituirlo por su consonante anterior, si fuera una c, sería una b y si fuera una p sería una ñ y si fuera una v sería una t
-  // Si el caracter es un espacio lo sustituirás por una letra random del alfabeto
-  
-  // let secretCode = '';
-  
-  // const alphabet = 'abcdefghijklmnñopqrstuvwxyz';
-  // const consonants = 'bcdfghjklmnñpqrstvwxyz';
-  
-  // for (const letter of phrase.toLowerCase()) {
-  // if (letter === 'a' || letter === 'á') {
-  // secretCode += 1;
-  // } else if (letter === 'e' || letter === 'é') {
-  // secretCode += 2;
-  // } else if (letter === 'i' || letter === 'í') {
-  // secretCode += 3;
-  // } else if (letter === 'o' || letter === 'ó') {
-  // secretCode += 4;
-  // } else if (letter === 'u' || letter === 'ú') {
-  // secretCode += 5;
-  // } else if (consonants.includes(letter)) {
-  // if (letter === 'b') {
-  // secretCode += 'z';
-  // } else {
-  // const position = consonants.indexOf(letter) - 1;
-  // secretCode += consonants.charAt(position);
-  // }
-  // } else if (letter === ' ') {
-  // const randomPosition = Math.floor(Math.random() * alphabet.length);
-  // secretCode += alphabet.charAt(randomPosition);
-  // } else {
-  // secretCode += letter;
-  // }
-  // }
-  
-  // console.log(secretCode);
-  // };
-  
-  // wordArray('Si no estudias acabarás como Victor');
+const data2 = {
+  firstFloor: {
+    vowels: []
+  },
+  secondFloor: {
+    constants: []
+  },
+
+  fourthFloor: {
+    asciiCode: []
+  },
+  fifthFloor: {
+    //Cada palabra de la frase será una posición del array
+    wordsInUppercase: [],
+    wordsInLowercase: []
+  },
+  sixthFloor: {
+    
+    secretCode: ''
+  }
+};
+
+const wordArray = phrase => {
+  const vowels = 'aeiouáéíóú';
+
+  for (const letter of phrase) {
+  if (vowels.includes(letter)) {
+  data2.firstFloor.vowels.push(letter);
+  } else if (letter !== ' ') {
+  data2.secondFloor.constants.push(letter);
+  }
+
+  data2.fourthFloor.asciiCode.push(letter.charCodeAt());
+  }
+
+  data2.fifthFloor.wordsInUppercase = phrase.toUpperCase().split(' ');
+  data2.fifthFloor.wordsInLowercase = phrase.toLowerCase().split(' ');
+
+// En este nivel codificarás la frase para que sea un secreto.
+// Si el caracter es una vocal, la sustituirás por un número siendo a-1 e-2 i-3 o-4 u-5
+// Si el caracter es una consonante deberás sustituirlo por su consonante anterior, si fuera una c, sería una b y si fuera una p sería una ñ y si fuera una v sería una t
+// Si el caracter es un espacio lo sustituirás por una letra random del alfabeto
+
+let secretCode = '';
+
+const alphabet = 'abcdefghijklmnñopqrstuvwxyz';
+const consonants = 'bcdfghjklmnñpqrstvwxyz';
+
+for (const letter of phrase.toLowerCase()) {
+if (letter === 'a' || letter === 'á') {
+secretCode += 1;
+} else if (letter === 'e' || letter === 'é') {
+secretCode += 2;
+} else if (letter === 'i' || letter === 'í') {
+secretCode += 3;
+} else if (letter === 'o' || letter === 'ó') {
+secretCode += 4;
+} else if (letter === 'u' || letter === 'ú') {
+secretCode += 5;
+} else if (consonants.includes(letter)) {
+if (letter === 'b') {
+secretCode += 'z';
+} else {
+const position = consonants.indexOf(letter) - 1;
+secretCode += consonants.charAt(position);
+}
+} else if (letter === ' ') {
+const randomPosition = Math.floor(Math.random() * alphabet.length);
+secretCode += alphabet.charAt(randomPosition);
+} else {
+secretCode += letter;
+}
+}
+
+console.log(secretCode);
+};
+
+wordArray('Si no estudias acabarás como Victor');
--- 7. CALLBACKS 10-06/JS/callbacks.js
+++ 7. CALLBACKS 10-06/JS/callbacks.js
@@ -41,3 +41,90 @@ const numbers = [1, 2, 3, 4, 5, 6];
 
 const even = numbers. filter(number => number % 2 === 0);
 console.log(even)
+
+// # Ejercicios
+
+// ## Arrays métodos callback
+
+// - Crea una función que reciba un array de números y muestre por consola cada número multiplicado por su índice en el array
+
+// - Crea una función que reciba un array de números y devuelva un array con cada número dividido por su índice en el array más 2, es decir index + 2
+
+// - Crea una función que reciba un array de palabras y devuelva un array con las mismas palabras en mayúsculas.
+
+// - Crea una función que reciba un array de palabras y una letra. La función devolverá un array con las palabras que comiencen por esa letra, si no hay mostrará un mensaje por consola diciendo que ninguna palabra coincide.
+
+// - Añade a la función anterior lo necesario para que funcione independientemente de mayusculas o minúsculas.
+
+// - Crea una función que reciba un array de 10 números e imprima por consola la suma de todos los valores del array.
+
+// - Crea una función que reciba un array de 10 números, imprime por consola cada número, su cuadrado y su cubo en este formato:
+//   "Número: 2 - Cuadrado: 4 - Cubo: 8".
+
+// - Crea una función que reciba una palabra e imprima por consola esa palabra pero con las vocales en mayúscula.
+
+// - Crea una función que reciba un array de 10 números. Dentro de esa función crea dos arrays llamados even (pares) y odd (impares), después multiplica cada uno de los números del array recibido por un número aleatorio entre 1 y 10, si el resultado es par, guárdalo en el array de pares, si es impar, en el array de impares, al final, imprime los 3 arrays por consola.
+
+// - Crea una función que reciba un array con 5 palabras, debes imprimir por consola un array que contenga la inicial y la última letra de cada palabra en mayúsculas, es decir, si nuestra función recibiera un array con ['hola', 'adios', 'gato', 'perro', 'casa'] deberá imprimir por consola ['H', 'A', 'A', 'S', 'G', 'O', 'P','O', 'C', 'A']. Si te quedas atascado puedes investigar la función flatMap() y flat()
+
+// - Crea una función que reciba un array de 10 números y te diga si alguno es mayor de 5.
+
+// - Crea una función que reciba un array de 5 palabras y un número y te devuelva un array con las palabras que tienen esa longitud, por ejemplo si le envias (['hola', 'adios', 'gato', 'perro', 'casa'], 4), te devolverá un array con ['hola', 'gato', 'casa']
+
+// - Crea una función que reciba un array de números y un número y te devuelva un array con los números que sean divisores de ese número
+
+// - Crea una función que reciba este array y te devuelva sólo los usuarios cuya edad sea menor de 30
+//   const array = [
+//   { name: 'John', age: 25 },
+//   { name: 'Jane', age: 30 },
+//   { name: 'Bob', age: 20 }
+//   ];
+
+// - Crea una función que reciba un array relleno con números y te diga si todos son pares o no.
+
+// - Crea una función que reciba un array de 5 palabras y las ordene en base a su longitud, de menor a mayor.
+
+// - Crea una función que reciba una palabra e imprima la misma palabra en orden inverso conservando las mayúsculas y las minúsculas. Si recibe "Mariposas" deberá imprimir "sasopiraM". No se puede usar reverse() 😊 (investiga la función reduceRight)
+
+// ## Retos!!
+
+// - Crea una función que reciba un array de 5 números de 2 dígitos. La función debe ser capaz de sumar los digitos de cada número, es decir si yo le envío [21, 34, 87, 10, 28] la función tendrá que ser capaz de devolverme un array con [3, 7, 15, 1, 10]
+
+// - Utilizando el array que te dejo a continuación resuelve estos dos retos.
+
+//   - Crea una función que reciba un criterio de ordenación y ordene el array en base a ese criterio. Puede ser el nombre, el apellido o la edad.
+
+//   - Crea una función que reciba un id de usuario y borre ese usuario del array.
+
+//     const users = [
+//     {
+//     id: "user001",
+//     name: "Juan",
+//     surname: "Pérez",
+//     age: 30
+//     },
+//     {
+//     id: "user002",
+//     name: "María",
+//     surname: "González",
+//     age: 25
+//     },
+//     {
+//     id: "user003",
+//     name: "Pedro",
+//     surname: "Sánchez",
+//     age: 35
+//     },
+//     {
+//     id: "user004",
+//     name: "Ana",
+//     surname: "Martínez",
+//     age: 28
+//     },
+//     {
+//     id: "user005",
+//     name: "Luis",
+//     surname: "López",
+//     age: 40
+//     }
+//     ];
